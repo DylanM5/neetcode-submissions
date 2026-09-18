da9fda3f9243cad41d89bd200a8b91397f2fd928
Dylan M <69550936+DylanM5@users.noreply.github.com>
2026-09-18 19:01:03 -0400

    Add: baseball-game - submission-0

---
+++ Data Structures & Algorithms/baseball-game/submission-0.cpp
@@ -0,0 +1,24 @@
+class Solution {
+   public:
+    int calPoints(vector<string>& operations) {
+        int score{0};
+        vector<int> stack{};
+
+        for (int i = 0; i < operations.size(); i++) {
+            if (operations[i] == "+") {
+                stack.push_back(stack[stack.size() - 1] + stack[stack.size() - 2]);
+            } else if (operations[i] == "C") {
+                stack.pop_back();
+            } else if (operations[i] == "D") {
+                stack.push_back(stack.back() * 2);
+            } else {
+                stack.push_back(stoi(operations[i]));
+            }
+        }
+        for (int i{0}; i < stack.size(); i++) {
+            score += stack[i];
+        }
+
+        return score;
+    }
+};
